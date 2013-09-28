ddcb94d48a4864be3fd4667c4024808e0859c13b
dbc-apprentice <apprentice@devbootcamp.com>
2013-09-27 19:13:11 -0500

    inches from MVP

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,6 +2,11 @@ get '/' do
   erb :index
 end
 
+get '/create' do
+  @user = current_user
+  erb :create
+end
+
 get '/logout' do
   session.clear
   erb :index
@@ -11,13 +16,45 @@ get '/register' do
   erb :register_new_user
 end
 
+get '/review/:id' do
+  @survey = Survey.find(params[:id])  
+  erb :review
+end
+
+get '/survey/:id' do
+  @survey = Survey.find(params[:id])
+  erb :survey
+end
+
+get '/survey/:id/results' do
+  @survey = Survey.find(params[:id])
+  erb :results
+end
+
 get '/profile/:id' do
   
 end
 
 
-
 #=============POST=============
+
+post '/review' do
+  @user = current_user
+  survey = Survey.create(name: params[:survey_name], user_id: @user.id)
+  question = Question.create(question: params[:survey_question])
+  question.choices << Choice.create(choice: params[:survey_choice_1])
+  question.choices << Choice.create(choice: params[:survey_choice_2])
+  question.choices << Choice.create(choice: params[:survey_choice_3])
+  survey.questions << question
+  redirect "/review/#{survey.id}"
+end
+
+post '/survey/:survey_id/results' do
+  @survey = Survey.find(params[:survey_id])
+  response = Response.create(response: params[:choice])
+  @survey.questions.first.choices.first.responses << response
+  redirect to ("/survey/#{@survey.id}/results")
+end
 
 post '/register' do
   @user = User.new(email: params[:email])
@@ -36,5 +73,4 @@ post '/login' do
   session[:user_id] = @user.id
   
   redirect "/profile/#{@user.id}"
-
 end
